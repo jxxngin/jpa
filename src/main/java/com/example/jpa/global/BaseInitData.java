5da3e207a80583e0d4483c260a904fca3108446a
jxxngin <wwd0203@daum.net>
2025-01-20 16:28:41 +0900

    [class] comment

--- src/main/java/com/example/jpa/global/BaseInitData.java
+++ src/main/java/com/example/jpa/global/BaseInitData.java
@@ -1,10 +1,10 @@
 package com.example.jpa.global;
 
+import com.example.jpa.domain.post.comment.entity.Comment;
+import com.example.jpa.domain.post.comment.service.CommentService;
 import com.example.jpa.domain.post.post.entity.Post;
 import com.example.jpa.domain.post.post.service.PostService;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +14,7 @@ import org.springframework.core.annotation.Order;
 @RequiredArgsConstructor
 public class BaseInitData {
     private final PostService postService;
+    private final CommentService commentService;
 
     @Bean
     @Order(1)
@@ -34,56 +35,12 @@ public class BaseInitData {
     @Bean
     @Order(2)
     public ApplicationRunner applicationRunner2() {
-        return new ApplicationRunner() {
-            @Override
-            @Transactional
-            public void run(ApplicationArguments args) throws Exception {
-                Post post = postService.findById(1L).get();
-                Thread.sleep(1000);
-                postService.modify(post, "new title1212", "new body1212");
-            }
-        };
-    }
+        return args -> {
+            Post post = postService.findById(1L).get();
 
-    // update, delete는 transaction이 끝날 때 일괄 처리된다.
-    @Bean
-    @Order(3)
-    public ApplicationRunner applicationRunner3() {
-        return new ApplicationRunner() {
-            @Override
-            @Transactional
-            public void run(ApplicationArguments args) throws Exception {
-                Post p1 = postService.findById(1L).get();
-                Post p2 = postService.findById(2L).get();
-
-                System.out.println("===== p1 삭제 =====");
-                postService.delete(p1);
-                System.out.println("===== p1 삭제 완료 =====");
-
-                System.out.println("===== p2 삭제 =====");
-                postService.delete(p2);
-                System.out.println("===== p2 삭제 완료 =====");
-            }
-        };
-    }
-
-    // 같은 트랜잭션 내에서 엔티티를 조회하면
-    // 최초 한번만 DB를 조회하고 이후에는 영속성 컨택스트에서 재사용
-    @Bean
-    @Order(4)
-    public ApplicationRunner applicationRunner4() {
-        return new ApplicationRunner() {
-            @Override
-            @Transactional
-            public void run(ApplicationArguments args) throws Exception {
-                Post post = postService.findById(3L).get();
-
-                System.out.println(post.getId() + "번 포스트를 가져왔습니다.");
-                System.out.println("==========================================");
-
-                Post post2 = postService.findById(3L).get();
-                System.out.println(post2.getId() + "번 포스트를 가져왔습니다.");
-            }
+            Comment c1 = commentService.write(post.getId(), "comment1");
+            Comment c2 = commentService.write(post.getId(), "comment2");
+            Comment c3 = commentService.write(post.getId(), "comment3");
         };
     }
 }
