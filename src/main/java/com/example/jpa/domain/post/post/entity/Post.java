8d523af9405e7e699b8b7aee14d60f5b1459f0c8
jxxngin <wwd0203@daum.net>
2025-01-20 11:31:03 +0900

    baseInitData

--- src/main/java/com/example/jpa/domain/post/post/entity/Post.java
+++ src/main/java/com/example/jpa/domain/post/post/entity/Post.java
@@ -1,14 +1,18 @@
 package com.example.jpa.domain.post.post.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Setter
+@Getter
 public class Post {
     @Id // PRIMARY KEY
     @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
-    private long id;
+    private Long id;    // long -> null X, Long -> null O
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
     @Column(length = 100)
